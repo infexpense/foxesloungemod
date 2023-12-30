b8ba3f0fb2e5cfac374a339ed9c9773db2d27449
Fuyuaki Vulpes <fuyuakivulpes@gmail.com>
2023-12-29 22:45:42 -0300

    Starting with the weapons

--- src/main/java/com/fuyuvulpes/yoamod/registries/ItemsModReg.java
+++ src/main/java/com/fuyuvulpes/yoamod/registries/ItemsModReg.java
@@ -1,5 +1,6 @@
 package com.fuyuvulpes.yoamod.registries;
 
+import com.fuyuvulpes.yoamod.core.ModToolTiers;
 import com.fuyuvulpes.yoamod.core.items.BaseModWeapon;
 import com.fuyuvulpes.yoamod.core.items.WeaponStats;
 import net.minecraft.world.food.FoodProperties;
@@ -16,19 +17,18 @@ public class ItemsModReg {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
 
 
-    public static final DeferredItem<BaseModWeapon> BASE_WEAPON = ITEMS.register(WeaponStats.PLACEHOLDER.name(), () -> new BaseModWeapon(Tiers.IRON,WeaponStats.PLACEHOLDER.attackDmg(),WeaponStats.PLACEHOLDER.attackSpeed(), WeaponStats.PLACEHOLDER.range(), new Item.Properties()));
-    public static final DeferredItem<BaseModWeapon> BASE_WEAPON2 = ITEMS.register(WeaponStats.PLACEHOLDER.name(), () -> new BaseModWeapon(Tiers.WOOD,WeaponStats.PLACEHOLDER.attackDmg(),WeaponStats.PLACEHOLDER.attackSpeed(), WeaponStats.PLACEHOLDER.range(), new Item.Properties()));
-    public static final DeferredItem<BaseModWeapon> BASE_WEAPON3 = ITEMS.register(WeaponStats.PLACEHOLDER.name(), () -> new BaseModWeapon(Tiers.WOOD,WeaponStats.PLACEHOLDER.attackDmg(),WeaponStats.PLACEHOLDER.attackSpeed(), WeaponStats.PLACEHOLDER.range(), new Item.Properties()));
-    public static final DeferredItem<BaseModWeapon> BASE_WEAPON4 = ITEMS.register(WeaponStats.PLACEHOLDER.name(), () -> new BaseModWeapon(Tiers.WOOD,WeaponStats.PLACEHOLDER.attackDmg(),WeaponStats.PLACEHOLDER.attackSpeed(), WeaponStats.PLACEHOLDER.range(), new Item.Properties()));
-    public static final DeferredItem<BaseModWeapon> BASE_WEAPON5 = ITEMS.register(WeaponStats.PLACEHOLDER.name(), () -> new BaseModWeapon(Tiers.WOOD,WeaponStats.PLACEHOLDER.attackDmg(),WeaponStats.PLACEHOLDER.attackSpeed(), WeaponStats.PLACEHOLDER.range(), new Item.Properties()));
-    public static final DeferredItem<BaseModWeapon> BASE_WEAPON6 = ITEMS.register(WeaponStats.PLACEHOLDER.name(), () -> new BaseModWeapon(Tiers.WOOD,WeaponStats.PLACEHOLDER.attackDmg(),WeaponStats.PLACEHOLDER.attackSpeed(), WeaponStats.PLACEHOLDER.range(), new Item.Properties()));
-    public static final DeferredItem<BaseModWeapon> BASE_WEAPON7 = ITEMS.register(WeaponStats.PLACEHOLDER.name(), () -> new BaseModWeapon(Tiers.WOOD,WeaponStats.PLACEHOLDER.attackDmg(),WeaponStats.PLACEHOLDER.attackSpeed(), WeaponStats.PLACEHOLDER.range(), new Item.Properties()));
-    public static final DeferredItem<BaseModWeapon> BASE_WEAPON8 = ITEMS.register(WeaponStats.PLACEHOLDER.name(), () -> new BaseModWeapon(Tiers.WOOD,WeaponStats.PLACEHOLDER.attackDmg(),WeaponStats.PLACEHOLDER.attackSpeed(), WeaponStats.PLACEHOLDER.range(), new Item.Properties()));
-    public static final DeferredItem<BaseModWeapon> BASE_WEAPON9 = ITEMS.register(WeaponStats.PLACEHOLDER.name(), () -> new BaseModWeapon(Tiers.WOOD,WeaponStats.PLACEHOLDER.attackDmg(),WeaponStats.PLACEHOLDER.attackSpeed(), WeaponStats.PLACEHOLDER.range(), new Item.Properties()));
-    public static final DeferredItem<BaseModWeapon> BASE_WEAPON10 = ITEMS.register(WeaponStats.PLACEHOLDER.name(), () -> new BaseModWeapon(Tiers.WOOD,WeaponStats.PLACEHOLDER.attackDmg(),WeaponStats.PLACEHOLDER.attackSpeed(), WeaponStats.PLACEHOLDER.range(), new Item.Properties()));
-    public static final DeferredItem<BaseModWeapon> BASE_WEAPON11 = ITEMS.register(WeaponStats.PLACEHOLDER.name(), () -> new BaseModWeapon(Tiers.WOOD,WeaponStats.PLACEHOLDER.attackDmg(),WeaponStats.PLACEHOLDER.attackSpeed(), WeaponStats.PLACEHOLDER.range(), new Item.Properties()));
-    public static final DeferredItem<BaseModWeapon> BASE_WEAPON12 = ITEMS.register(WeaponStats.PLACEHOLDER.name(), () -> new BaseModWeapon(Tiers.WOOD,WeaponStats.PLACEHOLDER.attackDmg(),WeaponStats.PLACEHOLDER.attackSpeed(), WeaponStats.PLACEHOLDER.range(), new Item.Properties()));
-    public static final DeferredItem<BaseModWeapon> BASE_WEAPON13 = ITEMS.register(WeaponStats.PLACEHOLDER.name(), () -> new BaseModWeapon(Tiers.WOOD,WeaponStats.PLACEHOLDER.attackDmg(),WeaponStats.PLACEHOLDER.attackSpeed(), WeaponStats.PLACEHOLDER.range(), new Item.Properties()));
+    public static final DeferredItem<BaseModWeapon> BRONZE_BELL = ITEMS.register("bronze_" + WeaponStats.BELL.name(), () -> new BaseModWeapon(ModToolTiers.BRONZE,WeaponStats.BELL.attackDmg(),WeaponStats.BELL.attackSpeed(), WeaponStats.BELL.range(), new Item.Properties()));
+    public static final DeferredItem<BaseModWeapon> GOLD_BELL = ITEMS.register("gold_" + WeaponStats.BELL.name(), () -> new BaseModWeapon(Tiers.GOLD,WeaponStats.BELL.attackDmg(),WeaponStats.BELL.attackSpeed(), WeaponStats.BELL.range(), new Item.Properties()));
+    public static final DeferredItem<BaseModWeapon> SILVER_BELL = ITEMS.register("silver_" + WeaponStats.BELL.name(), () -> new BaseModWeapon(ModToolTiers.SILVER,WeaponStats.BELL.attackDmg(),WeaponStats.BELL.attackSpeed(), WeaponStats.BELL.range(), new Item.Properties()));
+    public static final DeferredItem<BaseModWeapon> IRON_BELL = ITEMS.register("iron_" + WeaponStats.BELL.name(), () -> new BaseModWeapon(Tiers.IRON,WeaponStats.BELL.attackDmg(),WeaponStats.BELL.attackSpeed(), WeaponStats.BELL.range(), new Item.Properties()));
+    public static final DeferredItem<BaseModWeapon> STEEL_BELL = ITEMS.register("steel_" + WeaponStats.BELL.name(), () -> new BaseModWeapon(ModToolTiers.STEEL,WeaponStats.BELL.attackDmg(),WeaponStats.BELL.attackSpeed(), WeaponStats.BELL.range(), new Item.Properties()));
+    public static final DeferredItem<BaseModWeapon> BISMUTH_BELL = ITEMS.register("bismuth_" + WeaponStats.BELL.name(), () -> new BaseModWeapon(ModToolTiers.BISMUTH,WeaponStats.BELL.attackDmg(),WeaponStats.BELL.attackSpeed(), WeaponStats.BELL.range(), new Item.Properties()));
+    public static final DeferredItem<BaseModWeapon> DIAMOND_BELL = ITEMS.register("diamond_" + WeaponStats.BELL.name(), () -> new BaseModWeapon(Tiers.DIAMOND,WeaponStats.BELL.attackDmg(),WeaponStats.BELL.attackSpeed(), WeaponStats.BELL.range(), new Item.Properties()));
+    public static final DeferredItem<BaseModWeapon> NETHERITE_BELL = ITEMS.register("netherite_" + WeaponStats.BELL.name(), () -> new BaseModWeapon(Tiers.NETHERITE,WeaponStats.BELL.attackDmg(),WeaponStats.BELL.attackSpeed(), WeaponStats.BELL.range(), new Item.Properties()));
+    public static final DeferredItem<BaseModWeapon> TITANIUM_BELL = ITEMS.register("titanium_" + WeaponStats.BELL.name(), () -> new BaseModWeapon(ModToolTiers.TITANIUM,WeaponStats.BELL.attackDmg(),WeaponStats.BELL.attackSpeed(), WeaponStats.BELL.range(), new Item.Properties()));
+    public static final DeferredItem<BaseModWeapon> IOLITE_BELL = ITEMS.register("iolite_" + WeaponStats.BELL.name(), () -> new BaseModWeapon(ModToolTiers.IOLITE,WeaponStats.BELL.attackDmg(),WeaponStats.BELL.attackSpeed(), WeaponStats.BELL.range(), new Item.Properties()));
+    public static final DeferredItem<BaseModWeapon> ALEXANDRITE_BELL = ITEMS.register("alexandrite_" + WeaponStats.BELL.name(), () -> new BaseModWeapon(ModToolTiers.ALEXANDRITE,WeaponStats.BELL.attackDmg(),WeaponStats.BELL.attackSpeed(), WeaponStats.BELL.range(), new Item.Properties()));
+    public static final DeferredItem<BaseModWeapon> ADAMANTITE_BELL = ITEMS.register("adamantite_" + WeaponStats.BELL.name(), () -> new BaseModWeapon(ModToolTiers.ADAMANTITE,WeaponStats.BELL.attackDmg(),WeaponStats.BELL.attackSpeed(), WeaponStats.BELL.range(), new Item.Properties()));
 
 
 
