1a2d2e2c2ff1f0cf1b52ec760615ed3f4d8ac1f0
FuyuKyoshi <j-dauphinais@hotmail.com>
2024-04-12 12:13:45 -0500

    Peacock WIP animations needed

--- src/main/java/com/fuyuvulpes/yoamod/game/client/entities/model/PeafowlModel.java
+++ src/main/java/com/fuyuvulpes/yoamod/game/client/entities/model/PeafowlModel.java
@@ -16,24 +16,48 @@ import net.minecraft.world.entity.Entity;
 
 public class PeafowlModel<T extends Entity> extends HierarchicalModel<T> {
 	// This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
-	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("modid", "peacock"), "main");
-	private final ModelPart root;
+	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("modid", "peafowl"), "main");
+	private final ModelParts parts;
 
 	public PeafowlModel(ModelPart root) {
-		this.root = root.getChild("root");
+
+		ModelPart main = root.getChild("main");
+		ModelPart body = main.getChild("body");
+		ModelPart neck = body.getChild("neck");
+		ModelPart cube = neck.getChild("cube");
+		ModelPart head = neck.getChild("head");
+		ModelPart beak = head.getChild("beak");
+		ModelPart crest = head.getChild("crest");
+		ModelPart left_wing = body.getChild("left_wing");
+		ModelPart right_wing = body.getChild("right_wing");
+		ModelPart left_leg = body.getChild("left_leg");
+		ModelPart right_leg = body.getChild("right_leg");
+		ModelPart tail = body.getChild("tail");
+		ModelPart tail_feather1 = tail.getChild("tail_feather1");
+		ModelPart tail_feather2 = tail.getChild("tail_feather2");
+		ModelPart tail_feather3 = tail.getChild("tail_feather3");
+		ModelPart tail_feather4 = tail.getChild("tail_feather4");
+		ModelPart tail_feather5 = tail.getChild("tail_feather5");
+		ModelPart feather1 = tail_feather1.getChild("feather1");
+		ModelPart feather2 = tail_feather2.getChild("feather2");
+		ModelPart feather3 = tail_feather3.getChild("feather3");
+		ModelPart feather4 = tail_feather4.getChild("feather4");
+		ModelPart feather5 = tail_feather5.getChild("feather5");
+
+		this.parts = new ModelParts(main, body, neck, cube, head, beak, crest, left_wing, right_wing, left_leg, right_leg, tail, tail_feather1, tail_feather2, tail_feather3, tail_feather4, tail_feather5, feather1, feather2, feather3, feather4,feather5);
 	}
 
 	public static LayerDefinition createBodyLayer() {
 		MeshDefinition meshdefinition = new MeshDefinition();
 		PartDefinition partdefinition = meshdefinition.getRoot();
 
-		PartDefinition root = partdefinition.addOrReplaceChild("root", CubeListBuilder.create(), PartPose.offset(0.0F, 24.0F, 0.0F));
+		PartDefinition main = partdefinition.addOrReplaceChild("main", CubeListBuilder.create(), PartPose.offset(0.0F, 24.0F, 0.0F));
 
-		PartDefinition body = root.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 15).addBox(-2.5F, -8.0F, -4.0F, 5.0F, 5.0F, 7.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+		PartDefinition body = main.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 15).addBox(-2.5F, -8.0F, -4.0F, 5.0F, 5.0F, 7.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
 
 		PartDefinition neck = body.addOrReplaceChild("neck", CubeListBuilder.create(), PartPose.offset(0.0F, -5.8078F, -1.8294F));
 
-		PartDefinition cube_r1 = neck.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(20, 12).addBox(-1.0F, -3.5F, -1.0F, 2.0F, 7.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -3.0F, -2.0F, 0.2134F, 0.0F, 0.0F));
+		PartDefinition cube = neck.addOrReplaceChild("cube", CubeListBuilder.create().texOffs(20, 12).addBox(-1.0F, -3.5F, -1.0F, 2.0F, 7.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -3.0F, -2.0F, 0.2134F, 0.0F, 0.0F));
 
 		PartDefinition head = neck.addOrReplaceChild("head", CubeListBuilder.create().texOffs(12, 0).addBox(-1.5F, -2.0F, -3.0F, 3.0F, 2.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -5.1922F, -1.1706F));
 
@@ -53,23 +77,23 @@ public class PeafowlModel<T extends Entity> extends HierarchicalModel<T> {
 
 		PartDefinition tail_feather1 = tail.addOrReplaceChild("tail_feather1", CubeListBuilder.create(), PartPose.offset(0.0F, 8.0F, -3.0F));
 
-		PartDefinition cube_r2 = tail_feather1.addOrReplaceChild("cube_r2", CubeListBuilder.create().texOffs(26, 0).addBox(-2.5F, -12.1F, -1.9F, 3.0F, 13.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -6.0F, 3.0F, -2.0071F, 0.0F, 0.0F));
+		PartDefinition feather1 = tail_feather1.addOrReplaceChild("feather1", CubeListBuilder.create().texOffs(26, 0).addBox(-2.5F, -12.1F, -1.9F, 3.0F, 13.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -6.0F, 3.0F, -2.0071F, 0.0F, 0.0F));
 
 		PartDefinition tail_feather2 = tail.addOrReplaceChild("tail_feather2", CubeListBuilder.create(), PartPose.offset(0.0F, 8.0F, -3.0F));
 
-		PartDefinition cube_r3 = tail_feather2.addOrReplaceChild("cube_r3", CubeListBuilder.create().texOffs(26, 0).addBox(-2.0F, -12.3F, -1.7F, 3.0F, 13.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -6.0F, 3.0F, -2.0071F, 0.0F, 0.0F));
+		PartDefinition feather2 = tail_feather2.addOrReplaceChild("feather2", CubeListBuilder.create().texOffs(26, 0).addBox(-2.0F, -12.3F, -1.7F, 3.0F, 13.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -6.0F, 3.0F, -2.0071F, 0.0F, 0.0F));
 
 		PartDefinition tail_feather3 = tail.addOrReplaceChild("tail_feather3", CubeListBuilder.create(), PartPose.offset(0.0F, 8.0F, -3.0F));
 
-		PartDefinition cube_r4 = tail_feather3.addOrReplaceChild("cube_r4", CubeListBuilder.create().texOffs(26, 0).addBox(-1.5F, -12.5F, -1.5F, 3.0F, 13.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -6.0F, 3.0F, -2.0071F, 0.0F, 0.0F));
+		PartDefinition feather3 = tail_feather3.addOrReplaceChild("feather3", CubeListBuilder.create().texOffs(26, 0).addBox(-1.5F, -12.5F, -1.5F, 3.0F, 13.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -6.0F, 3.0F, -2.0071F, 0.0F, 0.0F));
 
 		PartDefinition tail_feather4 = tail.addOrReplaceChild("tail_feather4", CubeListBuilder.create(), PartPose.offset(0.0F, 8.0F, -3.0F));
 
-		PartDefinition cube_r5 = tail_feather4.addOrReplaceChild("cube_r5", CubeListBuilder.create().texOffs(26, 0).addBox(-1.0F, -12.3F, -1.7F, 3.0F, 13.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -6.0F, 3.0F, -2.0071F, 0.0F, 0.0F));
+		PartDefinition feather4 = tail_feather4.addOrReplaceChild("feather4", CubeListBuilder.create().texOffs(26, 0).addBox(-1.0F, -12.3F, -1.7F, 3.0F, 13.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -6.0F, 3.0F, -2.0071F, 0.0F, 0.0F));
 
 		PartDefinition tail_feather5 = tail.addOrReplaceChild("tail_feather5", CubeListBuilder.create(), PartPose.offset(0.0F, 8.0F, -3.0F));
 
-		PartDefinition cube_r6 = tail_feather5.addOrReplaceChild("cube_r6", CubeListBuilder.create().texOffs(26, 0).addBox(-0.5F, -12.1F, -1.9F, 3.0F, 13.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -6.0F, 3.0F, -2.0071F, 0.0F, 0.0F));
+		PartDefinition feather5 = tail_feather5.addOrReplaceChild("feather5", CubeListBuilder.create().texOffs(26, 0).addBox(-0.5F, -12.1F, -1.9F, 3.0F, 13.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -6.0F, 3.0F, -2.0071F, 0.0F, 0.0F));
 
 		return LayerDefinition.create(meshdefinition, 32, 32);
 	}
@@ -81,11 +105,14 @@ public class PeafowlModel<T extends Entity> extends HierarchicalModel<T> {
 
 	@Override
 	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
-		root.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+		this.parts.main.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+	}
+
+	private record ModelParts(ModelPart main, ModelPart body, ModelPart head, ModelPart neck, ModelPart cube, ModelPart beak, ModelPart crest, ModelPart left_wing, ModelPart right_wing, ModelPart left_leg, ModelPart right_leg, ModelPart tail, ModelPart tail_feather1, ModelPart tail_feather2, ModelPart tail_feather3, ModelPart tail_feather4, ModelPart tail_feather5, ModelPart feather1, ModelPart feather2, ModelPart feather3, ModelPart feather4, ModelPart feather5) {
 	}
 
 	@Override
 	public ModelPart root() {
-		return null;
+		return this.parts.main;
 	}
 }
--- src/main/java/com/fuyuvulpes/yoamod/game/client/entities/renderers/PeafowlRenderer.java
+++ src/main/java/com/fuyuvulpes/yoamod/game/client/entities/renderers/PeafowlRenderer.java
@@ -1,7 +1,10 @@
 package com.fuyuvulpes.yoamod.game.client.entities.renderers;
 
 import com.fuyuvulpes.yoamod.game.client.entities.model.PeafowlModel;
+import com.fuyuvulpes.yoamod.world.entity.FennecFoxEntity;
 import com.fuyuvulpes.yoamod.world.entity.PeafowlEntity;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.ResourceLocation;
@@ -17,6 +20,15 @@ public class PeafowlRenderer extends MobRenderer<PeafowlEntity, PeafowlModel<Pea
     }
     public PeafowlRenderer(EntityRendererProvider.Context pContext) {
         super(pContext, new PeafowlModel<>(pContext.bakeLayer(PeafowlModel.LAYER_LOCATION)), 0.4F);
+    }
+
+    @Override
+    public void render(PeafowlEntity pPea, float pEntityYaw, float pParticalTicks, PoseStack pMatrixStack, MultiBufferSource pBuffer, int pPackedLight){
+        if(pPea.isBaby()){
+            pMatrixStack.scale(0.6f, 0.6f, 0.6f);
+
+        }
+        super.render(pPea,pEntityYaw,pParticalTicks,pMatrixStack,pBuffer,pPackedLight);
     }
 
     public static ResourceLocation getVariantTexture(PeafowlEntity.Type pType) {
